5e40be9a7f5499c8d44c6cb4cb63040586ea6d9e
KcPele <fidekg123@gmail.com>
2022-05-27 13:22:49 +0100

    anagram

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
-class Student:
-    # [assignment] Skeleton class. Add your code here
-    def __init__(self, name, age, tracks, score):
-        self.name = name
-        self.age = age
-        self.tracks = tracks
-        self.score = score
-    
-    def change_name(self, name):
-        self.name = name
-    def change_age(self, age):
-        self.age = age
-    def add_track(self, track):
-        self.tracks.append(track)
-    def get_score(self):
-        return self.score
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
 
 
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
-
-# Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
-
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if(len(word) == len(anagram)):
+        if set(word) == set(anagram):
+            return True
+        return False
+        
+    return False
